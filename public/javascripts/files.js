fab0bb77017b9bedc46b3e80e9c81546e52e7d76
Catarinag16 <a85266@alunos.uminho.pt>
2020-12-24 16:45:15 +0000

    produtor: upload

---
+++ public/javascripts/files.js
@@ -0,0 +1,8 @@
+let addAutor = () => {
+    let fileInput = `
+            <div class="row mb-3">
+                <input class="form-control" type="text" placeholder="autor" name="autor" required="">
+            </div>
+    `;
+    $("#addAutor").append(fileInput);
+};
