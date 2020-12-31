47976987ef5716acb42e29629741ba812e792dcf
taniafilipa33 <com.tani33@gmail.com>
2020-12-31 10:45:45 +0000

    up view coments

--- public/javascripts/reply.js
+++ public/javascripts/reply.js
@@ -2,16 +2,20 @@
 function addi() {
   console.log("testeReply");
   var file = $(
-    "ul.w3-ul.w3-hoverable.w3-white" +
-      "each com in recurso.comentarios" +
-      "li.w3-padding-16" +
-      "span.w3-large=com.nome_utilizador" +
-      "br" +
-      "span.w3-medium=com.data_comentario" +
-      "br" +
-      "span=com.descricao" +
-      "#adiciona" +
-      "input(type='button' value='Mostrar replies' onclick='addi()')"
+    `
+    <ul class="w3-ul w3-hoverable w3-white">
+        each com in recurso.comentarios
+            <li class="w3-padding-16">
+                <span class="w3-large"> $com.nome_utilizador </span>
+                <br></br>
+                <span class="w3-medium"> $com.data_comentario </span>
+                <br></br>
+                <span> $com.descricao </span>
+                <div id="adiciona"
+                    <input type="button" value="Mostrar replies" onclick="addi()"/>
+                </div>
+            </li>
+    </ul>`
   );
   $("#adiciona").append(file);
 }
